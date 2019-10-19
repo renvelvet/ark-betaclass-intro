6e172f22b46c0ffad62a9cbd7c0075a4581c8ee2
Resha <reshapuspita734@gmail.com>
2019-10-19 17:12:43 +0700

    latihan tipe data

--- tipe-data.js
+++ tipe-data.js
@@ -1,2 +1,24 @@
-const data = 'holla'
-console.log(data)
+const string = 'ararara'
+const number = 123
+const bool = false
+const object = {nama:'renma', umur:21}
+const array= [1,2,3]
+const array1= ['a','b','c','d','e']
+const array2= [
+    {nama:'renma', umur:21},
+    {nama:'ran', umur:20},
+    {nama:'ren', umur:21}
+]
+
+console.log(`${string} ${number}`)
+console.log(`nama: ${object.nama}, umur: ${object.umur}`)
+
+for (i in array2) {
+    console.log(array2[i].nama)
+}
+
+const functionn = (obj) => {
+    console.log(`arrow function ${obj}`)
+}
+
+functionn('biru')
